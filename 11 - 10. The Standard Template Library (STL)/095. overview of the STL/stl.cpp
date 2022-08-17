43a5ff25307573b049e1d6490cb07a4c73a4109e
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-17 14:11:28 +0500

    Add files via upload

---
+++ 11 - 10. The Standard Template Library (STL)/095. overview of the STL/stl.cpp
@@ -0,0 +1,62 @@
+// overview of the Standard Template Library (STL)
+#include <cstdio>
+using namespace std;
+
+// The Standard Template Library is part of the ANSI ISO C++ Standard,
+// so it's guaranteed to be part of every C++ development system.
+// The Standard Template Library, commonly called the STL,
+// provides containers, and supporting data types
+// like vectors, lists, queues and iterators.
+// Other basic functions are provided by the C++ standard library.
+// The STL provides a number of standard containers, iterators and algorithms.
+// Some of these include,
+// -- vector -- STL
+// The vector, a fundamental, random access container
+// that can be used in many places a C Array can be used,
+// but with a lot more functionality.
+// -- list -- STL
+// The list, a double-linked list optimized for
+// rapid inserts and deletes.
+// -- set -- STL
+// A set, which is an ordered sequence container,
+// where elements are kept in a strict order
+// based on their value.
+// -- map -- STL
+// Map is an associative container,
+// where unique keys are used to
+// access associated values.
+// -- stack, queue, deque -- STL
+// Stack, queue and deque are LIFO, FIFO,
+// and double-ended containers for
+// pushing and popping data from a stack.
+// -- string -- STL
+// String is a specialized container
+// of characters optimized for use as strings.
+// -- I/O -- STL
+// I/O Streams read and write data
+// to and from files and I/O devices.
+//
+// It's worth noting that we used "cout"
+// for most of the console output.
+// cout << "Hello, World!" << endl;
+// We haven't beem using "cout" in other chapters
+// because outside the context of the STL,
+// "cout" is not efficient and
+// I don't tend to use it myself without the STL.
+// "cout" is designed to work with STL classes,
+// and I do use it when I'm using the STL.
+// And we use it extensively in this chapter.
+// "cout" is based on the I/O Stream classes.
+
+// The STL is very large.
+// Understanding the C++ Standard Template Library
+// is an essential part of becoming a competent C++ programmer.
+// The STL is part of the C++ standard, and it provides
+// a great deal of basic functionality for C++ programmers.
+
+int main(int argc, char **argv)
+{
+    puts("Standard Template Library");
+
+    return 0;
+}
