aa15c3b1684a4b2f654d5e73010214d42427b8db
Khasan Rashidov <89701590+khasanrashidov@users.noreply.github.com>
2022-08-17 14:33:18 +0500

    Add files via upload

---
+++ 12 - Conclusion/110. that is it, C++/cpp.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+using namespace std;
+
+// In this course, my goal was to gain
+// a good working knowledge of C++.
+// I've covered the basic syntax of the language,
+// along with its operators and data types,
+// and how to create my own functions,
+// classes, and templates.
+// I've learned how to use and understand
+// the powerful standard template library,
+// one of the most flexible and powerful
+// container libraries available in any language.
+// Along the way, I am sure I've taken some time to
+// experiment with exercise files and create
+// some of my own classes for use in my own projects.
+// C++ is a tremendously powerful language,
+// and while I've covered the essentials in this course,
+// it's not substitute for experience.
+// I need to write (encourage others as well) 
+// a lot of my own code and continue learning
+// from my own experiences with this
+// remarkable language. 
+// I've enjoyed this course, so I need to have fun
+// and create something wonderful in C++.
+
+int main(int argc, char **argv)
+{
+    puts("C++ Essential Training");
+    return 0;
+}
